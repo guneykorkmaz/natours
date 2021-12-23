47aa247945c631245f03b898081351b6f22c3bd5
Güney Korkmaz <guneyyk@gmail.com>
2021-12-23 14:28:40 +0300

    Prepared for deployment

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -4,7 +4,7 @@ import { showAlert } from './alerts';
 export const updateSettings = async (data, type) => {
   try {
     const url =
-      type === 'data' ? 'http://127.0.0.1:3000/api/v1/users/updateMe' : 'http://127.0.0.1:3000/api/v1/users/updatePassword';
+      type === 'data' ? '/api/v1/users/updateMe' : '/api/v1/users/updatePassword';
     const res = await axios({
       method: 'PATCH',
       url,
